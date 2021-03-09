3cfc8a9cbb9dcf60decc84ef65cc1e0c4614a26d
Aldibima30 <80263952+Aldibima30@users.noreply.github.com>
2021-03-09 11:00:20 +0700

    Update coba.php

--- coba.php
+++ coba.php
@@ -1,6 +1,5 @@
 <?php
-// Coded By N4ST4R_ID
-// www.nyamuxpl0it.xyz
+echo "<font color='#ff0000'>Red with code</font>
 error_reporting(E_ALL ^ (E_NOTICE | E_WARNING));
 echo "
   ___      _ ____
@@ -18,14 +17,14 @@ $wb = trim(fgets(STDIN));
 foreach($pat as $path) {
 $c3k = get_headers($wb.$path);
 if (preg_match("/200/", $c3k[0])) {
-echo "[200] Zeeb > $wb/$path \nDone Bosqueh \n";
+echo "[200] Zeeb > $wb/$path \nSudah Selesai Tuan \n";
 exit;
 } elseif (preg_match("/403/", $c3k[0])) {
-echo "[403] On > $wb/$path \nDone Bosqueh \n";
+echo "[403] On > $wb/$path \nSudah Selesai Tuan \n";
 exit;
 } else {
 echo "[-] Sangat mencintai nchi:) > $wb/$path \n";
 }
 }
-echo "Done bosqueh\n";
+echo "Sudah Selesai Tuan\n";
 ?>
